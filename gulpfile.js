dc6b5adc2d02a26e77946708f0b5487bf157ad5f
Lu-X-Liu <91901553+Lu-X-Liu@users.noreply.github.com>
2022-08-21 14:58:11 -0500

    get correct src image sizes

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const prefix = require('gulp-autoprefixer');
 const cleanCss = require('gulp-clean-css');
 const terser = require('gulp-terser');
 const rename = require('gulp-rename');
+const imgResize = require('gulp-image-resize'); 
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 // compile scss
 function scssTask() {
@@ -27,6 +30,67 @@ function js() {
       .pipe(browserSync.stream());
 }
 
+//optimize images
+const allImgs = {};
+
+const portrait = new Map([
+    ['small', 500],
+    ['medium', 750],
+    ['large', 1000]
+]);
+
+const landscape =new Map([
+    ['small', 500],
+    ['medium', 750],
+    ['large', 1000],
+    ['xlarge', 1300],
+    ['xxlarge', 1500]   
+])
+
+allImgs['portrait'] = portrait;
+allImgs['landscape'] = landscape;
+
+const srcDesign = 'src/imgs/design/';
+const distDesign = 'dist/imgs/design/';  
+
+const srcIllustration = 'src/imgs/illustration/';
+const distIllustration = 'dist/imgs/illustration/';
+
+function resizeAllImgs(srcDirPath, distDirPath, cb) {
+    for (const item in allImgs) {
+        allImgs[item].forEach((size, key) => {
+            src(srcDirPath + '*.jpg', {since: lastRun(resizeDisplayImgs)})
+            .pipe(        
+                imgResize({
+                width: size
+            }))
+            .pipe(rename(function (path) {
+                path.basename += '_' + key;
+            }))
+            .pipe(dest(distDirPath + key + '/' ))
+            .pipe(browserSync.stream());
+        }) 
+    }               
+    cb();
+}
+
+//optimize all images
+const imgSizes = ['small', 'medium', 'large', 'xlarge', 'xxlarge'];
+
+function optimizeAllImgs( distDirPath, cb) {
+    imgSizes.forEach((size) => {
+        src(distDirPath + size + '/*.jpg', {since: lastRun(optimizeDisplayImgs)})
+        .pipe(imagemin([
+        imagemin.mozjpeg({quality:75, progressive: true}),
+        imagemin.optipng({optimizationLevel: 3}),
+        ]))
+        .pipe(dest(distDirPath + size + '/'))
+        .pipe(browserSync.stream());       
+    })
+    cb();
+}
+
+//watch tasks
 function watchTasks() {
     browserSync.init({
         server: {
